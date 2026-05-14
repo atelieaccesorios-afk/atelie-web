4d0b8d72c2243984a18c23765ee03abc5cca6eb3
Atelie Joyeria <tuCorreo@gmail.com>
2026-05-14 14:46:13 -0500

    convert catalog images to webp

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const compromiso = Array.from({ length: 54 }, (_, i) => {
   return {
     id: i + 1,
     name: `Ref ${refNum}`,
-    img: process.env.PUBLIC_URL + `/catalogo/${refNum}.png`,
+    img: process.env.PUBLIC_URL + `/catalogo/${refNum}.webp`,
   };
 });
 
@@ -38,7 +38,7 @@ const matrimonio = Array.from({ length: 75 }, (_, i) => {
   return {
     id: 100 + i,
     name: `Ref ${refNum}`,
-    img: process.env.PUBLIC_URL + `/catalogo/${refNum}.png`,
+    img: process.env.PUBLIC_URL + `/catalogo/${refNum}.webp`,
   };
 });
 
